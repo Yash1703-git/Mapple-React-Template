de2e99d09dbd483f065b4b6d645e469a808f6f59
Yash1703-git <kumbharyash766@gmail.com>
2026-02-07 00:57:59 +0530

    Setup things

---
+++ src/Components/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div className='bg-yellow-400 text-white flex flex-col justify-center w-full border-t-2'>
+        <div className='w-[85%] bg-blue-50 mx-auto'>
+            <h1>Home</h1>
+        </div>  
+    </div>
+  )
+}
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div className='container w-[85%] mx-auto  bg-black text-white'>
+      Navbar
+    </div>
+  )
+}
